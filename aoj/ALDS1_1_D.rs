80fd057cf6c81147201be3e0da3e98cf61a862ff
Koji Ota <afterjnih@gmail.com>
2020-08-26 23:58:16 +0900

    refactoring

--- aoj/ALDS1_1_D.rs
+++ aoj/ALDS1_1_D.rs
@@ -1,33 +1,32 @@
-#![allow(unused)]
-
 #[cfg(local)]
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
 #[cfg(not(local))]
 use std::io::stdin;
-#[cfg(not(local))]
-use std::io::StdinLock;
+use std::io::Read;
 
 use std::cmp;
+use std::fmt::Debug;
 use std::io::BufReader;
 use std::str::FromStr;
 
-fn read_line<T: std::str::FromStr, U: std::io::Read>(buf: &mut BufReader<U>) -> T
+#[allow(dead_code)]
+fn read_line<T: FromStr, U: Read>(buf: &mut BufReader<U>) -> T
 where
-    <T as std::str::FromStr>::Err: std::fmt::Debug,
+    <T as FromStr>::Err: Debug,
 {
     let mut input = String::new();
-    buf.read_line(&mut input);
+    let _ = buf.read_line(&mut input);
     input.trim().parse().unwrap()
 }
 
-fn read_line_into_vec<T: std::str::FromStr, U: std::io::Read>(buf: &mut BufReader<U>) -> Vec<T>
+#[allow(dead_code)]
+fn read_line_into_vec<T: FromStr, U: Read>(buf: &mut BufReader<U>) -> Vec<T>
 where
-    <T as std::str::FromStr>::Err: std::fmt::Debug,
+    <T as FromStr>::Err: Debug,
 {
     let mut input = String::new();
-    buf.read_line(&mut input);
+    let _ = buf.read_line(&mut input);
     input
         .trim()
         .split_whitespace()
@@ -35,26 +34,26 @@ where
         .collect()
 }
 
-fn read_lines_to_end<T: std::str::FromStr, U: std::io::Read>(buf: &mut BufReader<U>) -> Vec<T>
+#[allow(dead_code)]
+fn read_lines_to_end<T: FromStr, U: Read>(buf: &mut BufReader<U>) -> Vec<T>
 where
-    <T as std::str::FromStr>::Err: std::fmt::Debug,
+    <T as FromStr>::Err: Debug,
 {
     let mut input = String::new();
-    buf.read_to_string(&mut input);
+    let _ = buf.read_to_string(&mut input);
     input
         .split_whitespace()
         .map(|e| e.parse().unwrap())
         .collect()
 }
 
-fn read_lines_to_end_into_vec<T: std::str::FromStr, U: std::io::Read>(
-    buf: &mut BufReader<U>,
-) -> Vec<Vec<T>>
+#[allow(dead_code)]
+fn read_lines_to_end_into_vec<T: FromStr, U: Read>(buf: &mut BufReader<U>) -> Vec<Vec<T>>
 where
-    <T as std::str::FromStr>::Err: std::fmt::Debug,
+    <T as FromStr>::Err: Debug,
 {
     let mut input = String::new();
-    buf.read_to_string(&mut input);
+    let _ = buf.read_to_string(&mut input);
     input
         .split_whitespace()
         .map({
@@ -78,7 +77,7 @@ fn main() {
     #[cfg(not(local))]
     let stdin = stdin();
     #[cfg(not(local))]
-    let mut handle = stdin.lock();
+    let handle = stdin.lock();
     #[cfg(not(local))]
     let mut reader = BufReader::new(handle);
 
@@ -87,7 +86,7 @@ fn main() {
 
     let mut min = input[0];
     let mut max = -9223372036854775808;
-    for i in (1usize..(len as usize)) {
+    for i in 1usize..(len as usize) {
         max = cmp::max(max, input[i] - min);
         min = cmp::min(min, input[i]);
     }
