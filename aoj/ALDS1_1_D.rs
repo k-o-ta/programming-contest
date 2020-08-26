bf2bfee5641afba8c06d474dc1da306284783cf1
Koji Ota <afterjnih@gmail.com>
2020-08-26 18:28:20 +0900

    update boilerplate

--- aoj/ALDS1_1_D.rs
+++ aoj/ALDS1_1_D.rs
@@ -1,52 +1,95 @@
-use std::cmp;
+#![allow(unused)]
+
 #[cfg(local)]
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-#[cfg(local)]
+#[cfg(not(local))]
+use std::io::stdin;
+#[cfg(not(local))]
+use std::io::StdinLock;
+
+use std::cmp;
 use std::io::BufReader;
 use std::str::FromStr;
 
-#[cfg(local)]
-fn buf_read<T: std::str::FromStr>() -> Vec<T> {
-    let f = File::open("input.txt").unwrap();
-    let input_lines: Vec<_> = BufReader::new(f).lines().collect();
-    input_lines
-        .into_iter()
-        .map(|e| e.unwrap().parse().ok().unwrap())
+fn read_line<T: std::str::FromStr, U: std::io::Read>(buf: &mut BufReader<U>) -> T
+where
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut input = String::new();
+    buf.read_line(&mut input);
+    input.trim().parse().unwrap()
+}
+
+fn read_line_into_vec<T: std::str::FromStr, U: std::io::Read>(buf: &mut BufReader<U>) -> Vec<T>
+where
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut input = String::new();
+    buf.read_line(&mut input);
+    input
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().unwrap())
         .collect()
 }
 
-#[cfg(not(local))]
-fn buf_read<T: std::str::FromStr>() -> Vec<T> {
-    let stdin = io::stdin();
-    let input_lines: Vec<_> = stdin.lock().lines().collect();
-    input_lines
-        .into_iter()
-        .map(|e| e.unwrap().parse().ok().unwrap())
+fn read_lines_to_end<T: std::str::FromStr, U: std::io::Read>(buf: &mut BufReader<U>) -> Vec<T>
+where
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut input = String::new();
+    buf.read_to_string(&mut input);
+    input
+        .split_whitespace()
+        .map(|e| e.parse().unwrap())
+        .collect()
+}
+
+fn read_lines_to_end_into_vec<T: std::str::FromStr, U: std::io::Read>(
+    buf: &mut BufReader<U>,
+) -> Vec<Vec<T>>
+where
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut input = String::new();
+    buf.read_to_string(&mut input);
+    input
+        .split_whitespace()
+        .map({
+            |line| {
+                line.trim()
+                    .split_whitespace()
+                    .map(|e| e.parse().unwrap())
+                    .collect()
+            }
+        })
         .collect()
 }
 
 fn main() {
     type Element = i64;
-    let input: Vec<Element> = buf_read();
-    // let mut input_vectors: Vec<Vec<Element>> = vec![];
-    // for input_line in input {
-    //     let input_str: Vec<String> = input_line.split(' ').map(String::from).collect();
-    //     let mut input_vector: Vec<Element> = vec![];
-    //     for input_elem_str in input_str {
-    //         let elem: Element = Element::from_str(input_elem_str.as_str()).unwrap();
-    //         input_vector.push(elem);
-    //     }
-    //     input_vectors.push(input_vector);
-    // }
-    // code
-    let mut min = input[1];
+    #[cfg(local)]
+    let file = File::open("input.txt").unwrap();
+    #[cfg(local)]
+    let mut reader = BufReader::new(file);
+
+    #[cfg(not(local))]
+    let stdin = stdin();
+    #[cfg(not(local))]
+    let mut handle = stdin.lock();
+    #[cfg(not(local))]
+    let mut reader = BufReader::new(handle);
+
+    let len: i64 = read_line(&mut reader);
+    let input: Vec<i64> = read_lines_to_end(&mut reader);
+
+    let mut min = input[0];
     let mut max = -9223372036854775808;
-    for i in (2usize..(input[0] as usize + 1)) {
+    for i in (1usize..(len as usize)) {
         max = cmp::max(max, input[i] - min);
         min = cmp::min(min, input[i]);
-        println!("min: {}, cur: {}", min, input[i]);
     }
     println!("{}", max);
 }
