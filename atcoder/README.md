9b199652d024d620c904963aceae561e690dd905
Koji Ota <afterjnih@gmail.com>
2020-09-21 16:36:31 +0900

    add atcoder

---
+++ atcoder/README.md
@@ -0,0 +1,5 @@
+* `cargo generate --git https://github.com/rust-lang-ja/atcoder-rust-base --branch ja`
+* Project Name: project_name
+* `cd project_name`
+* `RUSTFLAGS='--cfg local' cargo build`
+* `RUSTFLAGS='--cfg local' cargo run`
---
+++ atcoder/begginers-selection/src/placing_marbles.rs
@@ -0,0 +1,103 @@
+#[cfg(local)]
+use std::fs::File;
+use std::io::prelude::*;
+#[cfg(not(local))]
+use std::io::stdin;
+use std::io::Read;
+
+use std::fmt::Debug;
+use std::fmt::Display;
+use std::io::stdout;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::io::StdoutLock;
+use std::str::FromStr;
+
+#[allow(dead_code)]
+fn read_line<T: FromStr, U: Read>(reader: &mut BufReader<U>) -> T
+where
+    <T as FromStr>::Err: Debug,
+{
+    let mut input = String::new();
+    let _ = reader.read_line(&mut input);
+    input.trim().parse().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_line_into_vec<T: FromStr, U: Read>(reader: &mut BufReader<U>) -> Vec<T>
+where
+    <T as FromStr>::Err: Debug,
+{
+    let mut input = String::new();
+    let _ = reader.read_line(&mut input);
+    input
+        .trim()
+        .split_whitespace()
+        .map(|e| e.parse().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_lines_to_end<T: FromStr, U: Read>(reader: &mut BufReader<U>) -> Vec<T>
+where
+    <T as FromStr>::Err: Debug,
+{
+    let mut input = String::new();
+    let _ = reader.read_to_string(&mut input);
+    input
+        .split_whitespace()
+        .map(|e| e.parse().unwrap())
+        .collect()
+}
+
+#[allow(dead_code)]
+fn read_lines_to_end_into_vec<T: FromStr, U: Read>(reader: &mut BufReader<U>) -> Vec<Vec<T>>
+where
+    <T as FromStr>::Err: Debug,
+{
+    let mut input = String::new();
+    let _ = reader.read_to_string(&mut input);
+    input
+        .lines()
+        .map({
+            |line| {
+                line.split_whitespace()
+                    .map({ |e| e.parse().unwrap() })
+                    .collect()
+            }
+        })
+        .collect()
+}
+
+#[allow(dead_code)]
+fn buff_write<T: Display>(writer: &mut BufWriter<StdoutLock>, output: &T) {
+    let _ = writeln!(writer, "{}", output);
+}
+
+#[allow(dead_code)]
+fn vec_to_string<T: ToString>(vec: &Vec<T>) -> String {
+    vec.iter()
+        .map(|e| e.to_string())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn main() {
+    #[cfg(local)]
+    let file = File::open("input.txt").unwrap();
+    #[cfg(local)]
+    let mut reader = BufReader::new(file);
+
+    #[cfg(not(local))]
+    let stdin = stdin();
+    #[cfg(not(local))]
+    let handle = stdin.lock();
+    #[cfg(not(local))]
+    let mut reader = BufReader::new(handle);
+    let s: String = read_line(&mut reader);
+    println!(
+        "{}",
+        s.chars()
+            .fold(0, |acc, c| acc + c.to_digit(10).expect("is not digit"))
+    );
+}
